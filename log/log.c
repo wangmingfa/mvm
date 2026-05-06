93844f738bd98940db13f3d61a9575b1d50e0503
wangmingfa <91422761@qq.com>
2026-05-06 13:41:37 +0000

    fix: log.c兼容windows

--- log/log.c
+++ log/log.c
@@ -1,14 +1,32 @@
 #include <stdio.h>
-#include <unistd.h>
+
+#ifdef _WIN32
+  #include <windows.h>
+
+// 启用 Windows 10+ 虚拟终端（ANSI 转义序列支持）
+static void enable_virtual_terminal() {
+    HANDLE hOut = GetStdHandle(STD_OUTPUT_HANDLE);
+    if (hOut == INVALID_HANDLE_VALUE) return;
+    DWORD mode = 0;
+    if (!GetConsoleMode(hOut, &mode)) return;
+    SetConsoleMode(hOut, mode | ENABLE_VIRTUAL_TERMINAL_PROCESSING);
+}
+#endif
 
 // 进入备用屏幕
 void enter_alternate_screen() {
+#ifdef _WIN32
+    enable_virtual_terminal();
+#endif
     printf("\033[?1049h\033[2J\033[H");
     fflush(stdout);
 }
 
 // 退出备用屏幕
 void exit_alternate_screen() {
+#ifdef _WIN32
+    enable_virtual_terminal();
+#endif
     printf("\033[?1049l");
     fflush(stdout);
 }
