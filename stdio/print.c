d6e4e2a3753bfd7a048449697265f23eb6a104fb
wangmingfa <91422761@qq.com>
2026-05-21 14:05:53 +0000

    feat: 优化进度条

--- stdio/print.c
+++ stdio/print.c
@@ -4,6 +4,11 @@ void print(const char* s) {
     printf("%s", s);
 }
 
+// 刷新 stdout 缓冲区，确保进度条等不带换行的输出立即显示
+void flush_stdout() {
+    fflush(stdout);
+}
+
 // 只清除当前程序输出的 n 行，不影响终端历史输出
 void clear_lines(int n)
 {
